7cd3e3826452498da9d8bbda76d73c604aea1ab9
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-05-24 21:32:38 +0200

    Lauf-Animation hinzugefügt

--- character.py
+++ character.py
@@ -4,17 +4,25 @@ import os
 # Load Images
 player_image_dict = {}
 dir_assets_player = os.path.dirname("assets/player/")
-player_image_dict["standing_up"] = pygame.image.load(
-    os.path.join(dir_assets_player, "standing_up.png")
+player_image_dict["standing_up"] = (
+    pygame.image.load(os.path.join(dir_assets_player, "standing_up.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_up1.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_up2.png")),
 )
-player_image_dict["standing_down"] = pygame.image.load(
-    os.path.join(dir_assets_player, "standing_down.png")
+player_image_dict["standing_down"] = (
+    pygame.image.load(os.path.join(dir_assets_player, "standing_down.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_down1.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_down2.png")),
 )
-player_image_dict["standing_right"] = pygame.image.load(
-    os.path.join(dir_assets_player, "standing_right.png")
+player_image_dict["standing_right"] = (
+    pygame.image.load(os.path.join(dir_assets_player, "standing_right.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_right1.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_right2.png")),
 )
-player_image_dict["standing_left"] = pygame.image.load(
-    os.path.join(dir_assets_player, "standing_left.png")
+player_image_dict["standing_left"] = (
+    pygame.image.load(os.path.join(dir_assets_player, "standing_left.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_left1.png")),
+    pygame.image.load(os.path.join(dir_assets_player, "walking_left2.png")),
 )
 
 
@@ -27,6 +35,12 @@ class Character:
         self.row = 0
         self.walkCount = 0
         self.state = LookingUp(self)
+
+    def walk(self): # TODO: It's the right direction, but we aren't there yet.
+        if self.walkCount < 2:
+            self.walkCount += 1
+        else:
+            self.walkCount = 0
 
     def move_up(self):
         self.state = LookingUp(self)
@@ -61,7 +75,8 @@ class CharacterState:
 
 class LookingUp(CharacterState):
     def __init__(self, character: Character):
-        character.image = player_image_dict["standing_up"]
+        character.walk() # TODO: Maybe there is a better solution?
+        character.image = player_image_dict["standing_up"][character.walkCount]
         character.position_y -= character.step_size
         character.row += 1
 
@@ -80,7 +95,8 @@ class LookingUp(CharacterState):
 
 class LookingDown(CharacterState):
     def __init__(self, character: Character):
-        character.image = player_image_dict["standing_down"]
+        character.walk()
+        character.image = player_image_dict["standing_down"][character.walkCount]
         character.position_y += character.step_size
         character.row -= 1
 
@@ -99,7 +115,8 @@ class LookingDown(CharacterState):
 
 class LookingRight(CharacterState):
     def __init__(self, character: Character):
-        character.image = player_image_dict["standing_right"]
+        character.walk()
+        character.image = player_image_dict["standing_right"][character.walkCount]
         character.position_x += character.step_size
 
     def look_up(self, character: Character):
@@ -117,7 +134,8 @@ class LookingRight(CharacterState):
 
 class LookingLeft(CharacterState):
     def __init__(self, character: Character):
-        character.image = player_image_dict["standing_left"]
+        character.walk()
+        character.image = player_image_dict["standing_left"][character.walkCount]
         character.position_x -= character.step_size
 
     def look_up(self, character: Character):
