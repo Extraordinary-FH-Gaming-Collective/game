d5a81650b24c8292c4e7b184f027714320a41710
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2021-06-01 19:50:36 +0200

    wtf

--- game.py
+++ game.py
@@ -62,7 +62,8 @@ keyboard_control.assign_arrowkey_left(MoveLeftCommand(player))
         if abs(playerRectangle.top - carRectangle.bottom) < collision_tolerance:
             player.leben -=1
         if abs(playerRectangle.bottom - carRectangle.top) < collision_tolerance: 
-            player.leben -=1"""
+            player.leben -=1
+"""
 while running:
     # Game Loop
     clock.tick(SCREEN_FPS)
