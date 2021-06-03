a4ef53d350a72c232e65c4d8eb751ba91b4a504a
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2021-06-03 17:09:28 +0200

    feat: add invisble endzone surface for rendering

--- endzone.py
+++ endzone.py
@@ -4,8 +4,10 @@ import pygame
 class Endzone(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([50, 30])
-        self.image.fill((255, 0, 0))
+        self.image = pygame.Surface([20, 20], pygame.SRCALPHA, 32)
+        # self.image.fill((0, 0, 255))
+
+        self.image = self.image.convert_alpha()
         self.position_x = pos_x
         self.position_y = pos_y
         self.rect = self.image.get_rect()
@@ -16,3 +18,6 @@ class Endzone(pygame.sprite.Sprite):
     def update(self):
         self.rect.x = self.position_x
         self.rect.y = self.position_y
+
+    def reached(self, player):
+        self.image = player.image
