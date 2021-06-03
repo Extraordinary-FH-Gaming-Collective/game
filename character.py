429cc825043c2e922828c35c310178d9a20e4efc
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2021-06-03 16:07:49 +0200

    feat: create BounceBack State

--- character.py
+++ character.py
@@ -40,19 +40,19 @@ fence_bottom = FenceBottom()
 class Character(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
-        self.image = None
-        self.position_y = 715
-        self.position_x = 580
+        self.image = player_image_dict["standing_up"][0]
+        self.rect = self.image.get_rect()
+        self.position_y = CHARACTER_START_POSITION_Y
+        self.position_x = CHARACTER_START_POSITION_X
         self.step_size = CHARACTER_STEP_SIZE
         self.row = 0
         self.animationCount = 0
-        self.state = LookingUp(self)
+        self.state = None
+
         self.leben = 3
         self.herzImage = pygame.image.load(
             os.path.join(dir_assets_other, "pixelherz64_56.png")
         )
-        self.rect = self.image.get_rect()
 
     def walk(self):
         if self.animationCount < 2:
@@ -74,6 +74,9 @@ class Character(pygame.sprite.Sprite):
 
     def cheer(self):
         self.state = Cheering(self)
+
+    def bounce_back(self):
+        self.state = BounceBack(self)
 
     def render(self, screen):
         screen.blit(self.image, (self.position_x, self.position_y))
@@ -114,9 +117,9 @@ class LookingUp(CharacterState):
     def __init__(self, character: Character):
         character.walk()  # TODO: Maybe there is a better solution?
         character.image = player_image_dict["standing_up"][character.animationCount]
-        if character.position_y > fence_top.position_y:
+        if (character.position_y) > BORDER_TOP:
             character.position_y -= character.step_size
-        character.row += 1
+            character.row += 1
 
     def look_up(self, character: Character):
         pass
@@ -138,9 +141,9 @@ class LookingDown(CharacterState):
     def __init__(self, character: Character):
         character.walk()
         character.image = player_image_dict["standing_down"][character.animationCount]
-        if character.position_y < fence_bottom.position_y:
+        if character.position_y < BORDER_BOTTOM:
             character.position_y += character.step_size
-        character.row -= 1
+            character.row -= 1
 
     def look_up(self, character: Character):
         character.state = LookingUp(character)
@@ -159,7 +162,7 @@ class LookingRight(CharacterState):
     def __init__(self, character: Character):
         character.walk()
         character.image = player_image_dict["standing_right"][character.animationCount]
-        if character.position_x < (SCREEN_WIDTH - character.rect.w):
+        if character.position_x < (BORDER_RIGHT - character.rect.w):
             character.position_x += character.step_size
 
     def look_up(self, character: Character):
@@ -179,7 +182,7 @@ class LookingLeft(CharacterState):
     def __init__(self, character: Character):
         character.walk()
         character.image = player_image_dict["standing_left"][character.animationCount]
-        if character.position_x > 0:
+        if character.position_x > (BORDER_LEFT):
             character.position_x -= character.step_size
 
     def look_up(self, character: Character):
@@ -198,3 +201,8 @@ class LookingLeft(CharacterState):
 class Cheering(CharacterState):
     def __init__(self, character: Character):
         character.image = player_image_dict["cheering"]
+
+
+class BounceBack(CharacterState):
+    def __init__(self, character: Character):
+        character.position_y += 10
