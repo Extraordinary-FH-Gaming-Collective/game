b197be8d4f9827a94115a90d7342bda601f2ac70
Bot-Linus <82156103+Bot-Linus@users.noreply.github.com>
2021-06-06 01:17:26 +0200

    beauty

--- startscreen.py
+++ startscreen.py
@@ -1,8 +1,8 @@
 import pygame
- 
-mainClock = pygame.time.Clock()
 from pygame.locals import *
 from settings import *
+
+mainClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption(GAME_NAME)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -11,19 +11,14 @@ pygame.font.init()
 menu_font = pygame.font.SysFont(None, 50)
 header_font = pygame.font.SysFont(None, 70)
 
-
-
-
-#Textgenerierer 
+# Textgenerierer 
 def text_drawer(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
-
-
-#hier läuft später die Gameklasse drin
+# hier läuft später die Gameklasse drin
 def game():
     running = True
     while running:
@@ -35,13 +30,10 @@ def game():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
-        
         pygame.display.update()
         mainClock.tick(SCREEN_FPS)
  
-
-
-#hier wird die Anleitung generiert
+# hier wird die Anleitung generiert
 def introduction():
     running = True
     while running:
@@ -61,12 +53,8 @@ def introduction():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
-        
         pygame.display.update()
         mainClock.tick(SCREEN_FPS)
-
-
- 
 mouse_click = False
 
 def startscreen():
@@ -74,17 +62,14 @@ def startscreen():
         screen.blit(START_BACKGROUND_IMAGE, (0, 0)) 
         text_drawer("Frogger City", header_font, (COLOR_BLACK), screen, 495, 80) # Ich denke das könnte man eleganter Lösen
                                                                                  # Meine Versuche mit Screen_WIDTH/2 -Textlänge haben leider nicht geklappt
-
         mouse_x, mouse_y = pygame.mouse.get_pos()
-
-
-        #Buttonzeichnung
+        
+        # Buttonzeichnung
         start_button = pygame.Rect(SCREEN_WIDTH/2-100, 210, 200, 50)
         introduction_button = pygame.Rect(SCREEN_WIDTH/2-100, 330, 200, 50)
         game_exit_button = pygame.Rect(SCREEN_WIDTH/2-100, 470, 200, 50)
 
-
-        #Mausclick-Abfragen
+        # Mausclick-Abfragen
         if start_button.collidepoint((mouse_x, mouse_y)):
             if mouse_click:
                 game()
@@ -98,36 +83,34 @@ def startscreen():
                 pygame.quit()
                 exit()
 
-        #Zeichnung Button für den Start
+        # Zeichnung Button für den Start
         pygame.draw.rect(screen, (COLOR_GRAY_WHITE), start_button)
         text_drawer("Start", menu_font, (COLOR_BLACK), screen, 600, 220)
 
-        #Zeichnung Button für die Anleitung
+        # Zeichnung Button für die Anleitung
         pygame.draw.rect(screen, (COLOR_GRAY_WHITE), introduction_button)
         text_drawer("Anleitung", menu_font, (COLOR_BLACK), screen, 555, 340)
 
-        #Zeichnung Button für Spiel schließen
+        # Zeichnung Button für Spiel schließen
         pygame.draw.rect(screen, (COLOR_GRAY_WHITE), game_exit_button)
         text_drawer("Beenden", menu_font, (COLOR_BLACK), screen, 565, 480)
  
-        #Eingabeüberprüfer
+        # Eingabeüberprüfer
         mouse_click = False
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit()
+
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     exit()
+
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_click = True
-        
+                    
         pygame.display.update()
         mainClock.tick(SCREEN_FPS)
-
-
-
- 
 startscreen()
