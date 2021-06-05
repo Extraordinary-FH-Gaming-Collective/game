44a892ab3592bdafb7bdb9d78d464808ad6e0839
Bot-Linus <82156103+Bot-Linus@users.noreply.github.com>
2021-06-06 01:49:22 +0200

    beauty

--- startscreen.py
+++ startscreen.py
@@ -6,11 +6,11 @@ mainClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption(GAME_NAME)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.font.init() 
+pygame.font.init()
 menu_font = pygame.font.SysFont(None, 50)
 header_font = pygame.font.SysFont(None, 70)
 
-# Textgenerierer 
+# Textgenerierer
 def text_drawer(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
@@ -21,7 +21,7 @@ def text_drawer(text, font, color, surface, x, y):
 def game():
     running = True
     while running:
-        screen.blit(BACKGROUND_IMAGE, (0, 0)) 
+        screen.blit(BACKGROUND_IMAGE, (0, 0))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -31,12 +31,12 @@ def game():
                     running = False
         pygame.display.update()
         mainClock.tick(SCREEN_FPS)
- 
+
 # hier wird die Anleitung generiert
 def introduction():
     running = True
     while running:
-        screen.blit(START_BACKGROUND_IMAGE, (0, 0)) 
+        screen.blit(START_BACKGROUND_IMAGE, (0, 0))
         text_drawer("Anleitung", menu_font, (COLOR_BLACK), screen, 560, 30)
         text_drawer("Versuche alle Spieler rüber zubringen.", menu_font, (COLOR_BLACK), screen, 50, 150)
         text_drawer("Weiche den Autos aus und bleibe auf den Zügen.", menu_font, (COLOR_BLACK), screen, 50, 200)
@@ -58,7 +58,7 @@ mouse_click = False
 
 def startscreen():
     while True:
-        screen.blit(START_BACKGROUND_IMAGE, (0, 0)) 
+        screen.blit(START_BACKGROUND_IMAGE, (0, 0))
         text_drawer("Frogger City", header_font, (COLOR_BLACK), screen, 495, 80) # Ich denke das könnte man eleganter Lösen
                                                                                  # Meine Versuche mit Screen_WIDTH/2 -Textlänge haben leider nicht geklappt
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -93,7 +93,7 @@ def startscreen():
         # Zeichnung Button für Spiel schließen
         pygame.draw.rect(screen, (COLOR_GRAY_WHITE), game_exit_button)
         text_drawer("Beenden", menu_font, (COLOR_BLACK), screen, 565, 480)
- 
+
         # Eingabeüberprüfer
         mouse_click = False
         for event in pygame.event.get():
@@ -109,9 +109,9 @@ def startscreen():
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_click = True
-                    
+
         pygame.display.update()
         mainClock.tick(SCREEN_FPS)
 
-        
-startscreen()  
+
+startscreen()
