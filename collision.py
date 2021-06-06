eeacafa6a478be6ac3084351e06cfbfb3ba68c57
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-06 19:40:40 +0200

    Remove comments

--- collision.py
+++ collision.py
@@ -26,18 +26,5 @@ class CollisionHandler:
                                 character.position_x = 580
                                 character.leben -= 1
 
-            # jetzt wird eine Kollision entdeckt, sobald es eine Überschneidung gibt -> für Car-Lanes
-            # Return False setzen für -> Zug Lanes
-           
-            # rect1 = character
-            # rect2 = sprite
-
-            #if (rect1.x < rect2.x + rect2.width &&
-            #    rect1.x + rect1.width > rect2.x &&
-            #    rect1.y < rect2.y + rect2.height &&
-            #    rect1.y + rect1.height > rect2.y)
-            #    // collision detected!
-
-
         return False
       
