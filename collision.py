37493e6a9ec929fbe41353dbd12569a4c650b666
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-06 16:25:46 +0200

    Clean up the collision class

--- collision.py
+++ collision.py
@@ -2,8 +2,6 @@ class CollisionHandler:
     def __init__(self):
         self.hits = 0
 
-    
-    
     def check(self, lane, character):
         for sprite in lane.sprites:
             if lane.type == 'cars':
@@ -13,13 +11,12 @@ class CollisionHandler:
                             if character.position_y <= sprite.position_y +25:
                                 character.position_y = 715
                                 character.position_x = 580
-                                print(self.hits, 'Hits')
                                 self.hits += 1
                                 character.leben -=1
                                 if character.leben == 0:
-                                    pass
-                                #Game-Over-Overlay
-            #Zug-Lane, needs refactoring
+                                    pass  # Game-Over-Overlay
+                                
+            # Zug-Lane, needs refactoring
             if lane.type != 'cars':
                 if character.position_x <= sprite.position_x:
                     if character.position_x >= sprite.position_x+320:
@@ -27,7 +24,6 @@ class CollisionHandler:
                            if character.position_y >= sprite.position_y+34:
                                 character.position_y = 715
                                 character.position_x = 580
-                                print('daneben')
                                 character.leben -=1
 
             # jetzt wird eine Kollision entdeckt, sobald es eine Überschneidung gibt -> für Car-Lanes
