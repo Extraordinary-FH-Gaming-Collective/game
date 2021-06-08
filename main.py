faa3fb05db28b38a1865fa3c582af02328635a43
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-08 20:26:43 +0200

    Rename missing classes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from game import Game
 
-#  Compile this file to get the game started.
-#  Good luck and lots of fun!
+"""
+
+Compile this file to get the game started.
+
+Good luck and lots of fun!
+
+"""
+
 game = Game()
 
 while True:
