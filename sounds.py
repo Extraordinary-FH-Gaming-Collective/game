e4579d5a8864f811d0602a2746bb80cc54e7dd43
Sebastian <gumzinator@googlemail.com>
2021-06-08 22:56:47 +0200

    class sounds

--- sounds.py
+++ sounds.py
@@ -1,23 +1,24 @@
-import pygame
 
-background_music = ('assets/sounds/Jim Hall - Elsewhere.mp3')
-jump_sound = ('assets/sounds/Jump.mp3')
-finish_sound = ('assets/sounds/Finish.mp3')
-hit_sound = ("assets/sounds/Hit.mp3")
+class Sounds:
+    def __init__(self, game):
+        self.game = game
+        self.background_music = ('assets/sounds/Jim Hall - Elsewhere.mp3')
+        self.jump_sound = ('assets/sounds/Jump.mp3')
+        self.finish_sound = ('assets/sounds/Finish.mp3')
+        self.hit_sound = ("assets/sounds/Hit.mp3")
 
+    def loadMusic(self):
+        self.game.pygame.mixer.music.load(self.background_music)
+        self.game.pygame.mixer.Channel(0).play(self.pygame.mixer.Sound(self.background_music), -1)
 
-def loadMusic():
-    pygame.mixer.music.load(background_music)
-    pygame.mixer.Channel(0).play(pygame.mixer.Sound(background_music), -1)
+    def loadJump(self):
+        self.game.pygame.mixer.music.load(self.jump_sound)
+        self.game.pygame.mixer.music.play()
 
-def loadJump():
-    pygame.mixer.music.load(jump_sound)
-    pygame.mixer.music.play()
+    def loadFinish(self):
+        self.game.pygame.mixer.music.load(self.finish_sound)
+        self.game.pygame.mixer.music.play()
 
-def loadFinish():
-    pygame.mixer.music.load(finish_sound)
-    pygame.mixer.music.play()
-
-def loadHit():
-    pygame.mixer.music.load(hit_sound)
-    pygame.mixer.music.play()
+    def loadHit(self):
+        self.game.pygame.mixer.music.load(self.hit_sound)
+        self.game.pygame.mixer.music.play()
