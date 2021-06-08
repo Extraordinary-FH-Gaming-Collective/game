000644b45f3b1758e6b98841aa775468710a2a94
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-08 20:47:57 +0200

    Python style

--- collision.py
+++ collision.py
@@ -1,30 +1,27 @@
-"""
 
-This is where we detect any collisions.
-
-"""
 class CollisionHandler:
+    """ This is where we detect any collisions. """
 
-
-    """ Please move on. There nothing happening here. Really! """
     def __init__(self):
+        """ Please move on. There nothing happening here. Really! """
         pass
-
-    """ Check against a collision with a car or train.
-    In case the character / player is hit, we will call the hit method to handle that case.
     
-    Please note: As a hit by car may be obvious, it's a little different with trains:
 
-    We'll handle the case that the character is not on a train, as he jumped down to
-    the rails, as a hit and handle it equally as a hit by car.
-
-    """
+    
     def check(self, lane, character) -> None:
+        """ Check against a collision with a car or train.
+        In case the character / player is hit, we will call the hit method to handle that case.
+        
+        Please note: As a hit by car may be obvious, it's a little different with trains:
+
+        We'll handle the case that the character is not on a train, as he jumped down to
+        the rails, as a hit and handle it equally as a hit by car.
+        """
         if self.isHitByCar(character, lane) or self.isNotOnTrain(character, lane):
             character.hit()
 
-    """ Returns a boolean if hit by a car. """
     def isHitByCar(self, character, lane) -> bool:
+        """ Returns a boolean if hit by a car. """
         if lane.type != 'cars':
             return
 
@@ -34,12 +31,11 @@ class CollisionHandler:
 
         return False
 
-    """ Is the character not on the train? This would mean, he jumped onto the rails.
-
-    Living on the edge? Not tolerated in Frogger City.
-
-    """
     def isNotOnTrain(self, character, lane) -> bool:
+        """ Is the character not on the train? This would mean, he jumped onto the rails.
+
+        Living on the edge? Not tolerated in Frogger City.
+        """
         if lane.type == 'cars':
             return False
 
@@ -49,10 +45,10 @@ class CollisionHandler:
 
         return True
 
-    """ Checks if the character is on the right side from the left side the sprite. """
     def rightFromLeftEdge(self, character, sprite) -> bool:
+        """ Checks if the character is on the right side from the left side the sprite. """
         return character.getWidth() + character.position_x > sprite.position_x
 
-    """ Checks if the character is on the left side from the right side the sprite. """
     def leftFromRightEdge(self, character, sprite) -> bool:
+        """ Checks if the character is on the left side from the right side the sprite. """
         return sprite.position_x + sprite.getWidth() > character.position_x
