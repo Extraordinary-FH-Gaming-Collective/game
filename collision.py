1cdf649d8e6a3ceaded429e9111cd25b29497f18
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-06-08 20:38:54 +0200

    document the collision class

--- collision.py
+++ collision.py
@@ -1,12 +1,40 @@
+""" 
+
+This is where we detect any collisions. 
+
+"""
 class CollisionHandler:
+    
+    
+    """ 
+
+    Please move on. There nothing happening here. Really!
+
+    """
     def __init__(self):
         pass
 
-    def check(self, lane, character):
+    """ 
+
+    Check against a collision with a car or train. 
+    In case the character / player is hit, we will call the hit method to handle that case.
+    
+    Please note: As a hit by car may be obvious, it's a little different with trains:
+
+    We'll handle the case that the character is not on a train, as he jumped down to 
+    the rails, as a hit and handle it equally as a hit by car.
+
+    """
+    def check(self, lane, character) -> None:
         if self.isHitByCar(character, lane) or self.isNotOnTrain(character, lane):
             character.hit()
 
-    def isHitByCar(self, character, lane):
+    """ 
+
+    Returns a boolean if hit by a car. 
+
+    """
+    def isHitByCar(self, character, lane) -> bool:
         if lane.type != 'cars':
             return
 
@@ -16,7 +44,13 @@ class CollisionHandler:
 
         return False
 
-    def isNotOnTrain(self, character, lane):
+    """ 
+
+    Is the character not on the train? This would mean, he jumped onto the rails. 
+    Living on the edge? Not tolerated in Frogger city.
+
+    """
+    def isNotOnTrain(self, character, lane) -> bool:
         if lane.type == 'cars':
             return False
 
@@ -26,8 +60,19 @@ class CollisionHandler:
 
         return True
 
-    def rightFromLeftEdge(self, character, sprite):
+    """ 
+
+    Checks if the character is on the right side from the left side the sprite.
+
+    """
+    def rightFromLeftEdge(self, character, sprite) -> bool:
         return character.getWidth() + character.position_x > sprite.position_x
 
-    def leftFromRightEdge(self, character, sprite):
+
+    """ 
+
+    Checks if the character is on the left side from the right side the sprite.
+
+    """
+    def leftFromRightEdge(self, character, sprite) -> bool:
         return sprite.position_x + sprite.getWidth() > character.position_x
